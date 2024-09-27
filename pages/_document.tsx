ad8a243e5fa4a8676cea1d1be15ded78f85b76c2
jerald-lim <43735395+jerald-lim@users.noreply.github.com>
2024-09-28 06:29:53 +0800

    test collaborative highlights function

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,7 @@ export default function Document() {
   return (
     <Html lang="en">
       <Head />
-      <body>
+      <body data-highlights>
         <Main />
         <NextScript />
         <script src="https://cursor-party.jerald-lim.partykit.dev/cursors.js"></script>
